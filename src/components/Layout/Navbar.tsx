a74e0ff4a8a7338a4be99ca7e82da80b92b3c00c
Tudor Stroescu <tudorstroescu@yahoo.com>
2023-03-28 10:32:07 +0300

    added-catalog-route&link

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -1,69 +1,81 @@
-import React, {useState} from 'react'
-import {Menu, MenuProps} from "antd";
-import { MailOutlined, AppstoreOutlined, SettingOutlined } from '@ant-design/icons'
+import React, { useState } from "react";
+import { Menu, MenuProps } from "antd";
+import {
+  MailOutlined,
+  AppstoreOutlined,
+  SettingOutlined,
+} from "@ant-design/icons";
+import { Link } from "react-router-dom";
 
-const items: MenuProps['items'] = [
+const items: MenuProps["items"] = [
   {
-    label: 'Navigation One',
-    key: 'mail',
+    label: "Navigation One",
+    key: "mail",
     icon: <MailOutlined />,
   },
   {
-    label: 'Navigation Two',
-    key: 'app',
+    label: "Navigation Two",
+    key: "app",
     icon: <AppstoreOutlined />,
     disabled: true,
   },
   {
-    label: 'Navigation Three - Submenu',
-    key: 'SubMenu',
+    label: "Navigation Three - Submenu",
+    key: "SubMenu",
     icon: <SettingOutlined />,
     children: [
       {
-        type: 'group',
-        label: 'Item 1',
+        type: "group",
+        label: "Item 1",
         children: [
           {
-            label: 'Option 1',
-            key: 'setting:1',
+            label: "Option 1",
+            key: "setting:1",
           },
           {
-            label: 'Option 2',
-            key: 'setting:2',
+            label: "Option 2",
+            key: "setting:2",
           },
         ],
       },
       {
-        type: 'group',
-        label: 'Item 2',
+        type: "group",
+        label: "Item 2",
         children: [
           {
-            label: 'Option 3',
-            key: 'setting:3',
+            label: "Option 3",
+            key: "setting:3",
           },
           {
-            label: 'Option 4',
-            key: 'setting:4',
+            label: "Option 4",
+            key: "setting:4",
           },
         ],
       },
     ],
   },
   {
-    label: 'Catalog',
-    key: 'alipay',
+    label: <Link to="/catalog">Catalog</Link>,
+    key: "alipay",
   },
 ];
 
 function Navbar() {
-  const [current, setCurrent] = useState('mail');
+  const [current, setCurrent] = useState("mail");
 
-  const onClick: MenuProps['onClick'] = (e) => {
-    console.log('click ', e);
+  const onClick: MenuProps["onClick"] = (e) => {
+    console.log("click ", e);
     setCurrent(e.key);
   };
 
-  return <Menu onClick={onClick} selectedKeys={[current]} mode="horizontal" items={items} />;
+  return (
+    <Menu
+      onClick={onClick}
+      selectedKeys={[current]}
+      mode="horizontal"
+      items={items}
+    />
+  );
 }
 
 export default Navbar;
