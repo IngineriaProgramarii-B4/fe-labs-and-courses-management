784bb1455eadbbafbb2ac4c1b147ef018c2717e0
victorgrosu <vagrosu@yahoo.com>
2023-04-03 18:33:10 +0300

    Style user card

--- src/components/UserInfoModal/UserInfoModal.tsx
+++ src/components/UserInfoModal/UserInfoModal.tsx
@@ -1,76 +1,271 @@
-import React, { useState } from "react";
-import {Avatar, Input, Modal, Space} from "antd";
+import React, { useEffect, useState } from "react";
+import {
+  Avatar,
+  Button,
+  Divider,
+  Input,
+  Modal,
+  Space,
+  Spin,
+  Tooltip,
+  Upload,
+} from "antd";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 type UserInfoInputProps = {
-  title: string,
-  type: string,
-  value: string,
-  setValue: (val: string) => void
-}
+  title: string;
+  type: string;
+  value: string;
+  isEditing: boolean;
+  setValue?: (val: string) => void;
+};
 
-function UserInfoInput({title, type, value, setValue}: UserInfoInputProps) {
-  const [isEditing, setIsEditing] = useState(false);
+function UserInfoInput({
+  title,
+  type,
+  value,
+  setValue,
+  isEditing,
+}: UserInfoInputProps) {
+  const isInEditMode = !!(isEditing && setValue);
+
   return (
-    <div className={"flex"}>
-      <p className={"my-auto"}>{title}:</p>
-      {!isEditing ?
-        <div className={"flex"} onClick={() => setIsEditing(true)}>
-          <p className={"my-auto ml-2"}>{value}</p>
-          <i className={"fa-solid fa-pencil"}/>
-        </div>
-        :
+    <div className={"flex h-[2rem]"}>
+      <p className={"my-auto w-[5.5rem] text-end"}>{title}:</p>
+      {!isInEditMode ? (
+        <p className={"my-auto ml-3"}>{value}</p>
+      ) : (
         <div>
           <Input
             type={type}
-            className={"ml-2 w-[13rem]"}
+            className={"ml-2 w-[13rem] fill-black"}
             value={value}
-            onChange={e => setValue(e.target.value)}
+            onChange={(e) => setValue(e.target.value)}
           />
-          <i className={"fa-solid fa-check"} onClick={() => setIsEditing(false)}/>
         </div>
-      }
+      )}
     </div>
-  )
+  );
 }
 
-type UserInfoModalProps = {
-  userType: 'admin' | 'student' | 'teacher',
-  firstName: string,
-  lastName: string,
-  username: string,
-  email: string,
-  className?: string
-  registrationNumber?: number,
-  year?: number,
-  semester?: number,
+type UserDataType = {
+  firstName: string;
+  lastName: string;
+  username: string;
+  email: string;
+  registrationNumber?: string;
+  year?: number;
+  semester?: number;
 };
 
-function UserInfoModal({
-  userType,
-  lastName,
-  firstName,
-  username,
-  email,
-  className
-}: UserInfoModalProps) {
+const getDefaultUserData = (userType: "admin" | "student" | "teacher") => {
+  const userData: UserDataType = {
+    firstName: "",
+    lastName: "",
+    username: "",
+    email: "",
+  };
+  if (userType === "student") {
+    userData.registrationNumber = "";
+    userData.semester = 0;
+  }
+
+  return userData;
+};
+
+type UserInfoModalProps = {
+  userType: "admin" | "student" | "teacher";
+  avatar?: string;
+  className?: string;
+};
+
+function UserInfoModal({ userType, avatar, className }: UserInfoModalProps) {
+  const navigate = useNavigate();
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const [userData, setUserData] = useState<UserDataType>(
+    getDefaultUserData(userType)
+  );
+  const [newUsername, setNewUsername] = useState(userData.username);
+  const [newEmail, setNewEmail] = useState(userData.email);
+  const [newAvatar, setNewAvatar] = useState(avatar);
+
+  const uploadButton = (
+    <div className={"flex flex-col justify-center items-center"}>
+      <i className={"fas fa-plus font-s text-3xl"} />
+      <div className={"mt-2"}>Upload avatar</div>
+    </div>
+  );
+
+  useEffect(() => {
+    setNewUsername(userData.username);
+    setNewEmail(userData.email);
+  }, [userData]);
+
+  const avatarIconClickHandler = () => {
+    setIsLoading(true);
+    //mocking request data
+    setUserData({
+      firstName: "Olariu",
+      lastName: "Florin",
+      username: "olariuflorin",
+      email: "olariuflorin@gmail.com",
+      registrationNumber: "1234567890",
+      semester: 3,
+    });
+    setTimeout(() => setIsLoading(false), 1000);
+    setIsModalOpen(true);
+  };
+
+  const onSave = () => {
+    setIsEditing(false);
+    toast.success("User profile updated");
+  };
+
+  const onCancel = () => {
+    setIsEditing(false);
+    setNewUsername(userData.username);
+    setNewEmail(userData.email);
+    setNewAvatar(avatar);
+  };
+
+  const onLogout = () => {
+    //ToDo: logic for logging out
+    navigate("/login");
+  };
 
   return (
     <div className={`${className} cursor-pointer`}>
-      <Avatar icon={<i className={"fa-solid fa-user"}/>} shape={"square"} size={32} onClick={() => setIsModalOpen(true)}/>
+      <Avatar
+        icon={
+          avatar ? (
+            <img src={avatar} alt={"avatar"} />
+          ) : (
+            <i className={"fa-solid fa-user"} />
+          )
+        }
+        shape={"square"}
+        size={32}
+        onClick={avatarIconClickHandler}
+      />
       <Modal
-        title={"User Profile"}
+        title={
+          <span>
+            User Profile
+            {!isEditing && (
+              <Tooltip title={"Edit user"}>
+                <i
+                  className={"fa-solid fa-pencil ml-2 cursor-pointer"}
+                  onClick={() => setIsEditing(true)}
+                />
+              </Tooltip>
+            )}
+          </span>
+        }
         open={isModalOpen}
-        onCancel={() => setIsModalOpen(false)}
+        onCancel={() => {
+          setIsModalOpen(false);
+          onCancel();
+        }}
         destroyOnClose={true}
-        footer={null}
+        footer={
+          isEditing ? (
+            <>
+              <Button children={"Cancel"} onClick={onCancel} danger />
+              <Button children={"Save"} onClick={onSave} />
+            </>
+          ) : (
+            <Button children={"Logout"} onClick={onLogout} danger />
+          )
+        }
       >
-        <Space direction="vertical" size="middle" className={'d-flex px-[1rem] py-[2rem] w-full'}>
-          <UserInfoInput title={"First name"} value={firstName} setValue={() => {}} type={'text'} />
-          <UserInfoInput title={"Last name"} value={lastName} setValue={() => {}} type={'text'} />
-          <UserInfoInput title={"Username"} value={username} setValue={() => {}} type={'text'} />
-          <UserInfoInput title={"Email"} value={email} setValue={() => {}} type={'email'} />
-        </Space>
+        <div className={"w-full h-full px-[1rem] py-[1.5rem]"}>
+          <div
+            className={
+              "flex justify-center items-center w-[10rem] h-[10rem] mx-auto border-2 border-dotted rounded-2xl overflow-hidden"
+            }
+          >
+            {isEditing || !avatar ? (
+              <Upload>
+                {/*{newAvatar ? (*/}
+                {/*  <img*/}
+                {/*    src={newAvatar}*/}
+                {/*    alt="avatar"*/}
+                {/*    className={"object-cover"}*/}
+                {/*  />*/}
+                {/*) : (*/}
+                {uploadButton}
+                {/*)}*/}
+              </Upload>
+            ) : (
+              <img src={newAvatar} alt="avatar" className={"object-cover"} />
+            )}
+          </div>
+          <Divider dashed />
+          {!isLoading ? (
+            <Space direction="vertical" size={2} className={"flex w-full"}>
+              <UserInfoInput
+                title={"First name"}
+                value={userData.firstName}
+                type={"text"}
+                isEditing={isEditing}
+              />
+              <UserInfoInput
+                title={"Last name"}
+                value={userData.lastName}
+                type={"text"}
+                isEditing={isEditing}
+              />
+              <UserInfoInput
+                title={"Username"}
+                value={newUsername}
+                setValue={setNewUsername}
+                type={"text"}
+                isEditing={isEditing}
+              />
+              <UserInfoInput
+                title={"Email"}
+                value={newEmail}
+                setValue={setNewEmail}
+                type={"email"}
+                isEditing={isEditing}
+              />
+              {userType === "student" && (
+                <>
+                  {userData.registrationNumber && (
+                    <UserInfoInput
+                      title={"Registration"}
+                      value={userData.registrationNumber}
+                      type={"text"}
+                      isEditing={isEditing}
+                    />
+                  )}
+                  {userData.semester && (
+                    <>
+                      <UserInfoInput
+                        title={"Year"}
+                        value={Math.ceil(userData.semester / 2).toString()}
+                        type={"text"}
+                        isEditing={isEditing}
+                      />
+                      <UserInfoInput
+                        title={"Semester"}
+                        value={(((userData.semester + 1) % 2) + 1).toString()}
+                        type={"text"}
+                        isEditing={isEditing}
+                      />
+                    </>
+                  )}
+                </>
+              )}
+            </Space>
+          ) : (
+            <Spin tip="Loading..." className={"w-full my-5"} />
+          )}
+        </div>
       </Modal>
     </div>
   );
--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,9 +1,7 @@
-import React from 'react';
+import React from "react";
 
 function Login() {
-  return (
-    <div>Login (Bafta George)</div>
-  );
+  return <div>Login</div>;
 }
 
 export default Login;
