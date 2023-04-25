5cc32733cc46adaa6446911e267e9a2e3042b8cd
victorgrosu <vagrosu@yahoo.com>
2023-04-25 20:26:31 +0300

    Update tests

--- src/components/NetworkCard/NetworkCard.test.tsx
+++ src/components/NetworkCard/NetworkCard.test.tsx
@@ -1,43 +1,35 @@
+/* eslint-disable jest/no-conditional-expect */
 import React from "react";
-import { render, screen } from '@testing-library/react';
+import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
-import NetworkCard, { UserDataType } from './NetworkCard';
-import UserHeader, {UserHeaderProps} from "./UserHeader";
+import NetworkCard, { UserDataType } from "./NetworkCard";
+import UserHeader, { UserHeaderProps } from "./UserHeader";
 import UserInfoFields from "./UserInfoFields";
 
-window.matchMedia =
-  window.matchMedia ||
-  function () {
-    return {
-      matches: false,
-      addListener: function () {},
-      removeListener: function () {},
-    };
-  };
-
-describe("UserHeader", () =>{
+describe("UserHeader", () => {
   const userData = {
-    username:"dianacuzic",
-    firstname:"Diana",
-    lastname:"Cuzic"
+    username: "dianacuzic",
+    firstname: "Diana",
+    lastname: "Cuzic",
   };
 
-  test("should render properly", () =>{
+  test("should render properly", () => {
     render(
       <UserHeader
-      username={userData.username}
-      firstname={userData.firstname}
-      lastname={userData.lastname}
+        username={userData.username}
+        firstname={userData.firstname}
+        lastname={userData.lastname}
       />
     );
 
-    expect(screen.getByText("@" + userData.username)).toBeInTheDocument();  
-    expect(screen.getByText(`${userData.firstname} ${userData.lastname}`)).toBeInTheDocument();  
-
+    expect(screen.getByText("@" + userData.username)).toBeInTheDocument();
+    expect(
+      screen.getByText(`${userData.firstname} ${userData.lastname}`)
+    ).toBeInTheDocument();
   });
 });
 
-describe("NetworkCard dummy", () =>{
+describe("NetworkCard dummy", () => {
   const networkData = [
     {
       email: "diana.cuzic@gmail.com",
@@ -45,7 +37,7 @@ describe("NetworkCard dummy", () =>{
       lastName: "Cuzic",
       username: "dianacuzic",
       office: "P1",
-      department: "Secretary"
+      department: "Secretary",
     },
     {
       email: "stefan.ciobaca@uaic.com",
@@ -53,7 +45,7 @@ describe("NetworkCard dummy", () =>{
       lastName: "Ciobaca",
       username: "stefan.ciobaca",
       title: "Prof",
-      taughtSubjects: ["PA", "PF", "Logica"]
+      taughtSubjects: ["PA", "PF", "Logica"],
     },
     {
       email: "florin.eugen@uaic.ro",
@@ -63,52 +55,27 @@ describe("NetworkCard dummy", () =>{
       year: 2,
       semester: 4,
       registrationNumber: "123FAKE92929",
-      enrolledCourses: ["IP", "PA", "SGBD", "TW", "SE"]
-    }
+      enrolledCourses: ["IP", "PA", "SGBD", "TW", "SE"],
+    },
   ];
 
   test("should render properly", () => {
-    render(
-      <NetworkCard
-        email={networkData[0].email}
-        firstname={networkData[0].firstName}
-        lastname={networkData[0].lastName}
-        year={networkData[0].year}
-        semester={networkData[0].semester}
-        registrationNumber={networkData[0].registrationNumber}
-        enrolledCourses={networkData[0].enrolledCourses}
-      />
-    );
+    render(<NetworkCard />);
 
-    expect(screen.getByText(networkData[0].email)).toBeInTheDocument();  
-    expect(screen.getByText(networkData[0].firstName)).toBeInTheDocument();  
-    expect(screen.getByText(networkData[0].lastName)).toBeInTheDocument();  
-    expect(screen.getByText(networkData[0].year)).toBeInTheDocument();  
-    expect(screen.getByText(networkData[0].semester)).toBeInTheDocument();  
-    expect(screen.getByText(networkData[0].registrationNumber)).toBeInTheDocument();  
-    expect(screen.getByText(networkData[0].enrolledCourses[0])).toBeInTheDocument();  
+    networkData.forEach((data) => {
+      expect(screen.getByText(data.email)).toBeInTheDocument();
+      expect(screen.getByText(data.firstName)).toBeInTheDocument();
+      expect(screen.getByText(data.lastName)).toBeInTheDocument();
+      data.year && expect(screen.getByText(data.year)).toBeInTheDocument();
+      data.semester &&
+        expect(screen.getByText(data.semester)).toBeInTheDocument();
+      data.registrationNumber &&
+        expect(screen.getByText(data.registrationNumber)).toBeInTheDocument();
+      data.enrolledCourses &&
+        data.enrolledCourses[0] &&
+        expect(
+          screen.getByText(new RegExp(data.enrolledCourses[0], "i"))
+        ).toBeInTheDocument();
+    });
   });
 });
-
-// describe("NetworkCard fetch", () =>{
-//   const networkData = {
-//     username: "string",
-//     firstname: "string",
-//     lastname: "string"
-//   };
-
-//   test("should render properly", () =>{
-//     render(
-//       <UserHeader
-//       username={networkData.username}
-//       firstname={networkData.firstname}
-//       lastname={networkData.lastname}
-//       />
-//     );
-
-//     expect(screen.getByText(networkData.username)).toBeInTheDocument();  
-//     expect(screen.getByText(networkData.firstname)).toBeInTheDocument();  
-//     expect(screen.getByText(networkData.lastname)).toBeInTheDocument();  
-
-//   });
-// });
