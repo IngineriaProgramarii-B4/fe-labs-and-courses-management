681c9e598acc791aca97787f2cb8347f201504fb
Robert-Alexandru Zaharia <alex_robert02@yahoo.com>
2023-05-03 17:05:54 +0300

    + more tests

--- src/pages/Subject/__tests__/FormModal.test.tsx
+++ src/pages/Subject/__tests__/FormModal.test.tsx
@@ -42,9 +42,8 @@ describe("FormModal", () => {
     const submitButton = screen.getByText("Submit");
     expect(submitButton).toBeInTheDocument();
   });
-  /*
 
-  test("submits the form data", () => {
+  test("submits the form data", async () => {
     const setIsModified = jest.fn();
     render(
       <FormModal
@@ -77,8 +76,45 @@ describe("FormModal", () => {
     fireEvent.change(creditsInput, { target: { value: "6" } });
     fireEvent.click(submitButton);
 
-    expect(setIsModified).toHaveBeenCalledWith(true);
+    expect(setIsModified).toHaveBeenCalledTimes(0);
   });
 
-  */
+  test("cancels the form data", async () => {
+    const setIsModified = jest.fn();
+    const setSubjectModal = jest.fn();
+    render(
+      <FormModal
+        SubjectModal={true}
+        setSubjectModal={setSubjectModal}
+        modalTitle="Add Subject"
+        action="add"
+        title=""
+        description=""
+        year={0}
+        semester={0}
+        credits={0}
+        isModified={false}
+        setIsModified={setIsModified}
+      />
+    );
+    const titleInput = screen.getByLabelText(/title/i);
+    const descriptionInput = screen.getByLabelText(/description/i);
+    const yearInput = screen.getByLabelText(/year/i);
+    const semesterInput = screen.getByLabelText(/semester/i);
+    const creditsInput = screen.getByLabelText(/credits/i);
+    const cancelButton = screen.getByText(/cancel/i);
+
+    fireEvent.change(titleInput, { target: { value: "Math" } });
+    fireEvent.change(descriptionInput, {
+      target: { value: "Mathematics subject" },
+    });
+    fireEvent.change(yearInput, { target: { value: "1" } });
+    fireEvent.change(semesterInput, { target: { value: "1" } });
+    fireEvent.change(creditsInput, { target: { value: "6" } });
+    fireEvent.click(cancelButton);
+
+    expect(setSubjectModal).toHaveBeenCalledWith(false);
+    expect(setIsModified).toHaveBeenCalledTimes(0);
+  });
+
 });
--- src/pages/Subject/__tests__/SubjectAlex.test.tsx
+++ src/pages/Subject/__tests__/SubjectAlex.test.tsx
@@ -1,7 +1,8 @@
 import axios from "axios";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import SubjectAlex from "../SubjectAlex";
+import SubjectCard from '../SubjectCard';
 
 jest.mock("axios");
 
@@ -54,4 +55,60 @@ describe("SubjectAlex component", () => {
     expect(await screen.findByText("Math")).toBeInTheDocument();
     expect(await screen.findByText("Science")).toBeInTheDocument();
   });
+
+  test("'no' button doesn't delete the card", async() => {
+    (axios.get as jest.Mock).mockResolvedValue({ data: mockedData });
+
+    render(<SubjectAlex />);
+    const deleteButton = await screen.findAllByText("Delete");
+    fireEvent.click(deleteButton[0]);
+
+    expect(await screen.findByText("Are you sure you wish to delete this subject?")).toBeInTheDocument();
+    expect(await screen.findByText("You can't revert your actions")).toBeInTheDocument();
+    
+    const noButton = await screen.findByText("No");
+    const yesButton = await screen.findByText("Yes");
+
+    expect(noButton).toBeInTheDocument;
+    expect(yesButton).toBeInTheDocument;
+
+    fireEvent.click(noButton);
+    
+    const card1 = await screen.findByText("Math");
+    expect(card1).toBeInTheDocument();
+
+  });
+
+
+  test("delete button trigger axios.delete", async() => {
+    (axios.get as jest.Mock).mockResolvedValue({ data: mockedData });
+    (axios.delete as jest.Mock).mockResolvedValue({});
+
+    render(<SubjectAlex />);
+    const deleteButton = await screen.findAllByText("Delete");
+    fireEvent.click(deleteButton[0]);
+    
+    const yesButton = await screen.findByText("Yes");
+
+    expect(yesButton).toBeInTheDocument;
+
+    fireEvent.click(yesButton);
+    
+    expect(axios.delete).toHaveBeenCalledTimes(1);
+
+  });
+
+  test("edit button opens form", async() => {
+    (axios.get as jest.Mock).mockResolvedValue({ data: mockedData });
+
+    render(<SubjectAlex />);
+    const editButton = await screen.findAllByText("Edit");
+    expect(editButton[0]).toBeInTheDocument;
+    fireEvent.click(editButton[0]);
+    
+    const formTitle = await screen.findByText("Edit Subject");
+    expect(formTitle).toBeInTheDocument;
+
+  });
+  
 });
