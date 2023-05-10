947d563a47b40d50a910a721edbb3cc344a64abd
alexandraatrifan <alexandratrifan@yahoo.com>
2023-05-10 12:05:12 +0300

    404 test

--- src/components/TeacherInfo/TeacherInfoCard.test.tsx
+++ src/components/TeacherInfo/TeacherInfoCard.test.tsx
@@ -1,9 +1,10 @@
 import React from "react";
-import { act, render, screen, waitFor } from "@testing-library/react";
+import { act, render, screen, waitFor, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import CoursesCard, { CourseCard } from "./CourseCard";
 import TeacherInfoCard, { teacherDataProps } from "./TeacherInfoCard";
 import axios, { AxiosInstance } from "axios";
+import { CourseInputInfo } from "./CourseCard";
 
 jest.mock("axios");
 const axiosInstanceMock = axios as jest.Mocked<typeof axios>;
@@ -146,4 +147,70 @@ describe("CoursesCard", () => {
     expect(screen.getAllByText("yes")).toHaveLength(3);
     expect(screen.getAllByText("no")).toHaveLength(2);
   });
+});
+
+test("should log error on failed API request", async () => {
+  console.error = jest.fn(); // mock console.error function
+
+  const axiosInstance = axios.create({
+    baseURL: "http://localhost:8090/api/v1",
+    headers: {
+      "Content-Type": "application/json"
+    }
+  });
+
+  axiosInstance.get = jest.fn(() => Promise.reject(new Error("API request failed"))); // mock axiosInstance.get function to return a rejected promise
+
+  await expect(axiosInstance.get("/teachers?id=1")).rejects.toThrow(); // expect the rejected promise to throw an error
+
+  expect(console.error).toHaveBeenCalled(); // expect console.error to have been called
+});
+
+describe("Input CoursesCard", () => {
+
+  test("should be editable when in edit mode", () => {
+    const setValueMock = jest.fn();
+    render(
+      <CourseInputInfo
+        title={"title"}
+        value={"value"}
+        type={"type"}
+        setValue={setValueMock}
+      />
+    );
+    const inputField = screen.getByRole("textbox");
+    expect(inputField).toBeInTheDocument();
+
+    fireEvent.change(inputField, { target: { value: "test value" } });
+    expect(setValueMock).toHaveBeenCalledTimes(1);
+    expect(setValueMock).toHaveBeenCalledWith("test value");
+  });
+
+  // test("should make input editable on click", () => {
+  //   const { getByTestId } = render(<CourseInputInfo title="Test Title" type="text" value="Test Value" setValue={() => {}} />);
+  //   const editIcon = getByTestId("edit-icon");
+  //   fireEvent.click(editIcon);
+  //   const input = getByTestId("input");
+  //   expect(input).toBeVisible();
+  // });
+  // test("should update input value on change", () => {
+  //   const mockSetValue = jest.fn();
+  //   const { getByTestId } = render(<CourseInputInfo title="Test Title" type="text" value="Test Value" setValue={mockSetValue} />);
+  //   const editIcon = getByTestId("edit-icon");
+  //   fireEvent.click(editIcon);
+  //   const input = getByTestId("input");
+  //   fireEvent.change(input, { target: { value: "New Test Value" } });
+  //   expect(mockSetValue).toHaveBeenCalledWith("New Test Value");
+  // });
+  // test("should make input read-only and display updated value on check icon click", () => {
+  //   const { getByTestId, queryByTestId } = render(<CourseInputInfo title="Test Title" type="text" value="Test Value" setValue={() => {}} />);
+  //   const editIcon = getByTestId("edit-icon");
+  //   fireEvent.click(editIcon);
+  //   const checkIcon = getByTestId("check-icon");
+  //   fireEvent.click(checkIcon);
+  //   const readOnlyValue = queryByTestId("read-only-value");
+  //   expect(readOnlyValue).toHaveTextContent("Test Value");
+  //   const input = queryByTestId("input");
+  //   expect(input).not.toBeInTheDocument();
+  // });
 });
