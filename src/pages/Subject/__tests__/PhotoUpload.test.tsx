666897aff14c4f6c66f1e536ddad6970c360a552
Ana-Maria-C <95433627+Ana-Maria-C@users.noreply.github.com>
2023-05-10 17:06:42 +0300

    +photoupload-77

--- src/pages/Subject/__tests__/PhotoUpload.test.tsx
+++ src/pages/Subject/__tests__/PhotoUpload.test.tsx
@@ -5,9 +5,13 @@ import { UploadFile } from "antd/es/upload/interface";
 describe("PhotoUpload component", () => {
   const file: File = new File(["(⌐□_□)"], "test.png", { type: "image/png" });
   const fileList: UploadFile[] = [
-    { uid: "1", name: "test.png", status: "done", url: "http://localhost/test.png" },
+    {
+      uid: "1",
+      name: "test.png",
+      status: "done",
+      url: "http://localhost/test.png",
+    },
   ];
-  
 
   const setFileList = jest.fn();
   const setUpFile = jest.fn();
@@ -21,19 +25,19 @@ describe("PhotoUpload component", () => {
   };
 
   it("should render the component", () => {
-  const fileList : UploadFile[] = [];
-  const { getByText } = render(
-    <PhotoUpload
-      title="Test Title"
-      fileList={fileList}
-      setFileList={() => {}}
-      upFile={null}
-      setUpFile={() => {}}
-    />
-  );
+    const fileList: UploadFile[] = [];
+    const { getByText } = render(
+      <PhotoUpload
+        title="Test Title"
+        fileList={fileList}
+        setFileList={() => {}}
+        upFile={null}
+        setUpFile={() => {}}
+      />
+    );
 
-  const uploadPhotoText = getByText("Upload Photo");
-  expect(uploadPhotoText).toBeInTheDocument();
+    const uploadPhotoText = screen.getByText("Upload Photo");
+    expect(uploadPhotoText).toBeInTheDocument();
   });
 
   it("renders an upload button with text when fileList is empty", () => {
@@ -47,13 +51,59 @@ describe("PhotoUpload component", () => {
       />
     );
 
-    expect(getByText("Upload Photo")).toBeInTheDocument();
+    expect(screen.getByText("Upload Photo")).toBeInTheDocument();
   });
 
   it("should call onPreview when a picture is clicked", () => {
     window.open = jest.fn();
     const { getByText } = render(<PhotoUpload {...props} />);
-    fireEvent.click(getByText("test.png"));
+    fireEvent.click(screen.getByText("test.png"));
     expect(window.open).toHaveBeenCalled();
   });
+  /***************************************************************************************************/
+  const props2 = {
+    title: "Upload your photo",
+    fileList: [],
+    setFileList: jest.fn(),
+    upFile: null,
+    setUpFile: jest.fn(),
+  };
+  /*
+  it("should render with a title and upload button", () => {
+    const { getByText } = render(<PhotoUpload {...props2} />);
+    expect(screen.getByText(props2.title)).toBeInTheDocument();
+    expect(screen.getByText("Upload Photo")).toBeInTheDocument();
+  });
+*/
+  it("should allow the user to select and upload a file", async () => {
+    const { getByTestId } = render(<PhotoUpload {...props2} />);
+    const file = new File(["file contents"], "test.png", {
+      type: "image/png",
+    });
+    const input = screen
+      .getByTestId("upload-input")
+      .querySelector('input[type="file"]') as HTMLInputElement;
+    fireEvent.change(input, { target: { files: [file] } });
+    expect(props2.setFileList).toHaveBeenCalledWith([{ originFileObj: file }]);
+    expect(props2.setUpFile).toHaveBeenCalledWith(file);
+  });
+  /*
+  it("should allow the user to preview an uploaded file", async () => {
+    const file = {
+      uid: "123",
+      name: "test.png",
+      status: "done",
+      url: "data:image/png;base64,...",
+    };
+    const { getByTestId, getByRole } = render(
+      <PhotoUpload {...props2} fileList={[file]} />
+    );
+    const uploadCard = screen
+      .getByTestId("upload-input")
+      .querySelector(".ant-upload-list-item") as HTMLElement;
+    fireEvent.click(uploadCard);
+    const previewImage = screen.getByRole("img") as HTMLImageElement;
+    expect(previewImage.src).toEqual(file.url);
+  });
+  */
 });
