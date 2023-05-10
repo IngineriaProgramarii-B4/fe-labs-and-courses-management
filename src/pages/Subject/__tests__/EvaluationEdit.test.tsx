58bfd25d5a1616d1a2bbe7f0552cd58cf27bb529
Robert-Alexandru Zaharia <alex_robert02@yahoo.com>
2023-05-10 04:06:14 +0300

    + EvaluationEdit test 91,3%

--- src/pages/Subject/__tests__/EvaluationEdit.test.tsx
+++ src/pages/Subject/__tests__/EvaluationEdit.test.tsx
@@ -1,34 +1,42 @@
 import { render, waitForElementToBeRemoved } from "@testing-library/react";
 import EvaluationEdit from "../EvaluationEdit";
-import { fireEvent, screen } from "@testing-library/react";
+import { fireEvent, screen, waitFor } from "@testing-library/react";
 import axios from "axios";
 
-test("renders EvaluationEdit component without crashing", async() => {
-    const setIsModified = jest.fn();
-    const setIsVisibile = jest.fn();
+jest.mock("axios");
+
+describe("FileTransfer component", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("calls the onCancel function and updates state correctly", () => {
+    const setIsVisible = jest.fn();
     const setResetFields = jest.fn();
+
     render(
       <EvaluationEdit
         subjectTitle="Math"
         title="Course"
         isModified={false}
-        setIsModified={setIsModified}
+        setIsModified={() => {}}
         description="Exam"
         value={0.5}
         isVisible={true}
-        setIsVisible={setIsVisibile}
+        setIsVisible={setIsVisible}
         resetFields={false}
         setResetFields={setResetFields}
       />
     );
-    fireEvent.click(screen.getByText("Close"));
-    expect(setIsVisibile).toHaveBeenCalledTimes(1);
-    const closeButton = screen.queryByText("Close")
-    expect (closeButton).toBeNull;
 
+    const cancelButton = screen.getByText("Close");
+    fireEvent.click(cancelButton);
+
+    expect(setIsVisible).toHaveBeenCalledWith(false);
+    expect(setResetFields).toHaveBeenCalledWith(true);
   });
 
-  test("calls axios.put with correct data when save button is clicked", async () => {
+  it("calls axios.put with correct data when save button is clicked", async () => {
     const setIsModified = jest.fn();
     const setIsVisible = jest.fn();
     const setResetFields = jest.fn();
@@ -55,15 +63,55 @@ test("renders EvaluationEdit component without crashing", async() => {
     const saveButton = screen.getByText("Save");
     fireEvent.click(saveButton);
   
-    await waitForElementToBeRemoved(() => screen.getByText("Edit Course"));
+    //await waitForElementToBeRemoved(() => screen.getByText("Edit Course"));
   
-    expect(axiosPutMock).toHaveBeenCalledTimes(1);
+    await waitFor(() => expect(axiosPutMock).toHaveBeenCalledTimes(1));
     expect(axiosPutMock).toHaveBeenCalledWith(
       "http://localhost:8090/api/v1/subjects/Math/evaluationMethods/component=Course",
       { component: "Course", description: "New description", value: 0.5 }
     );
     expect(setIsModified).toHaveBeenCalledTimes(1);
     expect(setIsVisible).toHaveBeenCalledTimes(1);
+    axiosPutMock.mockRestore(); // restore the original `axios.put()` method
   });
+
+  it("logs an error message when axios.put throws an error", async () => {
+    const setIsModified = jest.fn();
+    const setIsVisible = jest.fn();
+    const setResetFields = jest.fn();
+    const axiosPutMock = jest.spyOn(axios, "put").mockRejectedValue(new Error("Mock error"));
+    const consoleSpy = jest.spyOn(console, "error").mockImplementation(() => {});
   
+    render(
+      <EvaluationEdit
+        subjectTitle="Math"
+        title="Course"
+        isModified={false}
+        setIsModified={setIsModified}
+        description="Exam"
+        value={0.5}
+        isVisible={true}
+        setIsVisible={setIsVisible}
+        resetFields={false}
+        setResetFields={setResetFields}
+      />
+    );
+      
+    const saveButton = screen.getByText("Save");
+    fireEvent.click(saveButton);
   
+    await waitFor(() => expect(axiosPutMock).toHaveBeenCalledTimes(1));
+    expect(axiosPutMock).toHaveBeenCalledWith(
+      "http://localhost:8090/api/v1/subjects/Math/evaluationMethods/component=Course",
+      { component: "Course", description: "Exam", value: 0.5 }
+    );
+    expect(setIsModified).not.toHaveBeenCalled();
+    expect(setIsVisible).not.toHaveBeenCalled();
+    expect(setResetFields).not.toHaveBeenCalled();
+    expect(screen.queryByText("Edit Course")).toBeInTheDocument();
+  
+    axiosPutMock.mockRestore();
+    consoleSpy.mockRestore();
+  });
+
+});
