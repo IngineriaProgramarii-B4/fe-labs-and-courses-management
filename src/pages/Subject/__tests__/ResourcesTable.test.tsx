b37c18a65ead81ed80d29f82cbc0bd66e158631d
Robert-Alexandru Zaharia <alex_robert02@yahoo.com>
2023-05-10 16:12:12 +0300

    + ResourceTable 84%

--- src/pages/Subject/__tests__/ResourcesTable.test.tsx
+++ src/pages/Subject/__tests__/ResourcesTable.test.tsx
@@ -11,12 +11,30 @@ describe('ResourcesTable', () => {
     (axios.get as jest.Mock).mockResolvedValue({ data: [] });
   });
 
-  test('renders the component', async () => {
+  test('rendering ResourceTable', async () => {
     render(<ResourcesTable component="Course" title="example" />);
-    const modal = await screen.findByTestId('add-button');
     const addButton = await screen.findByTestId('add-button');
-    expect(modal).toBeInTheDocument();
     expect(addButton).toBeInTheDocument();
+
+    const modal = screen.queryByTestId('modal');
+    expect(modal).toBeNull();
+
+    fireEvent.click(addButton);
+    await waitFor(() => {
+      const modal = screen.getByTestId('modal');
+      expect(modal).toBeInTheDocument();
+    });
+    
+    const okButton = screen.getByTestId('ok-add-button');
+    expect(okButton).toBeInTheDocument();
+
+    fireEvent.click(okButton);
+
+    await waitFor(() => {
+      const modal = screen.queryByTestId('modal');
+      expect(modal).toBeNull;
+    });
+
   });
 
   test('deletes a resource on button click', async () => {
@@ -54,9 +72,12 @@ describe('ResourcesTable', () => {
       expect(axios.get).toHaveBeenCalled();
       expect(screen.queryByText('Resource 1')).not.toBeInTheDocument();
     });
+
+
   });
 
-  test('opens the file on resource link click', async () => {
+
+  test("close modal on 'no'", async () => {
     const data = [
       {
         key: '1',
@@ -76,15 +97,91 @@ describe('ResourcesTable', () => {
     // Mock the window.open method
     window.open = jest.fn();
 
+    const deleteButton = screen.getByTestId("delete-icon");
+    fireEvent.click(deleteButton);
+
+    expect(screen.getByText('Are you sure you wish to delete this resource?')).toBeInTheDocument();
+
+    fireEvent.click(screen.getByText('No'));
+
+    await waitFor(() => {
+      expect(screen.getByText('Resource 1')).toBeInTheDocument();
+    });
+  });
+
+
+  test('throws an error when delete', async () => {
+    const data = [
+      {
+        key: '1',
+        title: 'Resource 1',
+        timeStamp: '2023-05-10',
+        type: 'pdf',
+      },
+    ];
+
+    (axios.get as jest.Mock).mockResolvedValueOnce({ data });
+
+    const axiosDeleteMock = jest.spyOn(axios, "delete").mockRejectedValue(new Error("Mock error"));
+    const consoleSpy = jest.spyOn(console, "error").mockImplementation(() => {});
+
+    render(<ResourcesTable component="example" title="example" />);
+    await waitFor(() => {
+      expect(screen.getByText('Resource 1')).toBeInTheDocument();
+    });
+
+    // Mock the window.open method
+    window.open = jest.fn();
+
+    const deleteButton = screen.getByTestId("delete-icon");
+    fireEvent.click(deleteButton);
+
+    expect(screen.getByText('Are you sure you wish to delete this resource?')).toBeInTheDocument();
+
+    fireEvent.click(screen.getByText('Yes'));
+
+    expect(axiosDeleteMock).toHaveBeenCalledWith(
+      'http://localhost:8090/api/v1/subjects/example/components/example/resources/title=Resource 1'
+    );
+
+    await waitFor(() => {
+      expect(axios.get).toHaveBeenCalled();
+    });
+
+    axiosDeleteMock.mockRestore();
+    consoleSpy.mockRestore();
+    
+  });
+
+
+
+  test('opens the file on resource link click', async () => {
+    const data = [
+      {
+        key: '1',
+        title: 'Resource 1',
+        timeStamp: '2023-05-10',
+        type: 'pdf',
+      },
+    ];
+
+    (axios.get as jest.Mock).mockResolvedValueOnce({ data });
+
+    render(<ResourcesTable component="example" title="example" />);
+    await waitFor(() => {
+      expect(screen.getByText('Resource 1')).toBeInTheDocument();
+    });
+
+    window.open = jest.fn();
+
     fireEvent.click(screen.getByText('Resource 1'));
 
-    // Assert that the axios.get method is called with the correct URL
     expect(axios.get).toHaveBeenCalledWith(
       'http://localhost:8090/api/v1/subjects/example/components/example/resources/file=Resource 1',
       { responseType: 'arraybuffer' }
     );
-
-    // Assert that the window.open method is called with the correct URL
     //expect(window.open).toHaveBeenCalled();
   });
+
+
 });
