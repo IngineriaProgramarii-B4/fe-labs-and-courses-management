1db770e99ad80dece3ceda6b5b9a84935e3e89ec
alexandraatrifan <alexandratrifan@yahoo.com>
2023-05-20 18:39:28 +0300

    HomePage update

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,16 +1,23 @@
 import React from "react";
 import Navbar from "../../components/Layout/Navbar";
 import styles from "./Home.module.scss";
+import students_img from "./students.jpeg";
+
 
 
 function Home() {
   return (
     <>
-    <div className={styles.container}>
-      <Navbar></Navbar>
-      <div className={styles.backgroundImage}/>
-     
-    </div>
+      <div className={styles.container + " " + styles.bodyHome}>
+        <div className={styles.shadow}><Navbar></Navbar></div>
+        <div className={styles.name}> <b>uniManager</b></div>
+        <div className={styles.description}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        </div>
+        <div className={styles.students_img + " " + styles.shadow}>
+          <img src={students_img} alt="Students" />
+        </div>
+      </div>
     </>
   );
 }
