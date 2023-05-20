970656368888dff2d1369c5c4463130dfc96c43c
alexandraatrifan <alexandratrifan@yahoo.com>
2023-05-20 15:22:07 +0300

    update HomePage

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Navbar from "../../components/Layout/Navbar";
-import './Home.scss';
+import './Home.css';
 
 function Home() {
   return (
     <div>
       <Navbar></Navbar>
     <div className="body"> Home</div>
+    {/* <img src="students.jpeg" alt=""></img> */}
     </div>
   );
 }
