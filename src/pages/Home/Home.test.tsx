d279066878e4f15282f8c60244ade44d0763376f
victorgrosu <vagrosu@yahoo.com>
2023-05-29 14:52:55 +0300

    Remove unused route

--- src/pages/Home/Home.test.tsx
+++ src/pages/Home/Home.test.tsx
@@ -3,14 +3,20 @@ import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import Home from "./Home";
 
-test("renders Home component", () => {
-  render(<Home />);
-  expect(screen.getByText("uniManager")).toBeInTheDocument();
-  expect(screen.getByText("Unlock your academic potential with our all-in-one university app. Connect, learn, and thrive in one seamless platform!")).toBeInTheDocument();
-});
+describe("Home", () => {
+  test("renders Home component", () => {
+    render(<Home />);
+    expect(screen.getByText("uniManager")).toBeInTheDocument();
+    expect(
+      screen.getByText(
+        "Unlock your academic potential with our all-in-one university app. Connect, learn, and thrive in one seamless platform!"
+      )
+    ).toBeInTheDocument();
+  });
 
-test("renders image", () => {
+  test("renders image", () => {
     render(<Home />);
     const imageElement = screen.getByAltText("Students");
     expect(imageElement).toBeInTheDocument();
   });
+});
